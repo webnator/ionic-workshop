8f0c6851c9a4342d1c4fc61d5bd2de1950721f1f
Williams <webnator@gmail.com>
2018-10-15 14:54:52 +0200

    ADD: Added step-3 functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,7 +5,37 @@ import { Component } from '@angular/core';
   templateUrl: 'home.html',
 })
 export class HomePage {
+  items: Array<any> = [];
 
   constructor() {}
+
+  get pendingItems() {
+    return this.items.filter(item => !item.done)
+  }
+  get doneItems() {
+    return this.items.filter(item => item.done)
+  }
+
+  addNewItem(value: string): void {
+    if (value) {
+      this.items.push({
+        id: this.randomNumber(),
+        done: false,
+        description: value,
+      });
+    }
+  }
+
+  removeFromList(item: any): void {
+    if (item.done) {
+      this.items.splice(this.items.findIndex(it => it.id === item.id), 1);
+    } else {
+      alert('No puedes borrar un item no completado');
+    }
+  }
+
+  private randomNumber(): number {
+    return Math.floor((Math.random() * 1000) + 1);
+  }
 
 }
